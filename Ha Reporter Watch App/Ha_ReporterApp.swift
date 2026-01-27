1e3498957c89ec8dd9a7ca4f1f604b432312e24a
crazywoola <427733928@qq.com>
2026-01-27 16:01:59 +0800

    Initial Commit

---
+++ Ha Reporter Watch App/Ha_ReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ha_ReporterApp.swift
+//  Ha Reporter Watch App
+//
+//  Created by 蕉泥座人 on 2026/1/27.
+//
+
+import SwiftUI
+
+@main
+struct Ha_Reporter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
